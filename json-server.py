39ac00a991c3d1032dba0da97c6d0eb3f773a22a
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-02-10 19:20:03 -0600

    Added nss_handler & json-server files, initalized db

---
+++ json-server.py
@@ -0,0 +1,120 @@
+import json
+from http.server import HTTPServer
+from nss_handler import HandleRequests, status
+
+# Add your imports below this line
+from views import user
+from views import create_user, login_user
+
+class JSONServer(HandleRequests):
+    """Server class to handle incoming HTTP requests for shipping ships"""
+
+    def do_GET(self):
+        """Handle GET requests from a client"""
+
+        response_body = ""
+        url = self.parse_url(self.path)
+
+        if url["requested_resource"] == "user":
+            # Example workflow for get user by id
+            # if url["pk"] != 0:
+            #     response_body = retrieve_user(url["pk"])
+            #     return self.response(response_body, status.HTTP_200_SUCCESS.value)
+
+            # response_body = list_users()
+            # return self.response(response_body, status.HTTP_200_SUCCESS.value)
+            pass
+
+        else:
+            return self.response(
+                "", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+            )
+
+    def do_PUT(self):
+        """Handle PUT requests from a client"""
+
+        # Parse the URL and get the primary key
+        url = self.parse_url(self.path)
+        pk = url["pk"]
+
+        # Get the request body JSON for the new data
+        content_len = int(self.headers.get("content-length", 0))
+        request_body = self.rfile.read(content_len)
+        request_body = json.loads(request_body)
+
+    # Example of updating user info
+    #     if url["requested_resource"] == "user":
+    #         if pk != 0:
+    #             successfully_updated = update_user(pk, request_body)
+    #             if successfully_updated:
+    #                 return self.response(
+    #                     "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+    #                 )
+
+        return self.response(
+            "Requested resource not found",
+            status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+        )
+
+    def do_DELETE(self):
+        """Handle DELETE requests from a client"""
+
+        url = self.parse_url(self.path)
+        pk = url["pk"]
+
+        if url["requested_resource"] == "user":
+            pass
+        # Example of deleting a user
+    #         if pk != 0:
+    #             successfully_deleted = delete_user(pk)
+    #             if successfully_deleted:
+    #                 return self.response(
+    #                     "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+    #                 )
+
+    #             return self.response(
+    #                 "Requested resource not found",
+    #                 status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+    #             )
+
+        else:
+            return self.response(
+                "Not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+            )
+
+    def do_POST(self):
+        """Handle POST requests from a client"""
+
+        response_body = ""
+        url = self.parse_url(self.path)
+
+        content_len = int(self.headers.get("content-length", 0))
+        request_body = self.rfile.read(content_len)
+        request_body = json.loads(request_body)
+
+        # Register a new user
+        if url["requested_resource"] == "register":
+            response_body = create_user(request_body)
+            return self.response(response_body, status.HTTP_201_SUCCESS_CREATED.value)
+        
+        # Login a user
+        elif url["requested_resource"] == "login":
+            response_body = login_user(request_body)
+            return self.response(response_body, status.HTTP_201_SUCCESS_CREATED.value)
+
+        else:
+            return self.response(
+                "", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+            )
+
+#
+# THE CODE BELOW THIS LINE IS NOT IMPORTANT FOR REACHING YOUR LEARNING OBJECTIVES
+#
+def main():
+    host = ""
+    port = 8000
+    HTTPServer((host, port), JSONServer).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
