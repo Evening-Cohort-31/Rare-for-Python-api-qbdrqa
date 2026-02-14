da686d52792d3ae3d28a5dd942c8f8e8a80ae9cb
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-02-14 11:54:01 -0600

    Refactor code formatting and improve readability in JSONServer class

--- json-server.py
+++ json-server.py
@@ -3,9 +3,20 @@ from http.server import HTTPServer
 from nss_handler import HandleRequests, status
 
 # Add your imports below this line
-from views import create_user, login_user, create_post, get_user_posts, get_post_by_id, update_post, get_all_posts, get_user
+from views import (
+    create_user,
+    login_user,
+    create_post,
+    get_user_posts,
+    get_post_by_id,
+    update_post,
+    get_all_posts,
+    get_user,
+)
 from views import get_unapproved_posts, approve_post
 from views import get_all_categories, create_category, get_category_by_id
+
+
 class JSONServer(HandleRequests):
     """Server class to handle incoming HTTP requests for shipping ships"""
 
@@ -25,8 +36,8 @@ class JSONServer(HandleRequests):
             # return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
         elif url["requested_resource"] == "posts":
-            if url["pk"] !=0:
-                response_body = get_post_by_id(url['pk'])
+            if url["pk"] != 0:
+                response_body = get_post_by_id(url["pk"])
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
             if "user_id" in query_params:
                 user_id = query_params["user_id"][0]
@@ -64,12 +75,12 @@ class JSONServer(HandleRequests):
         request_body = json.loads(request_body)
 
         if url["requested_resource"] == "posts":
-            if pk !=0:
+            if pk != 0:
                 if "approved" in request_body and len(request_body) == 1:
                     response_body = approve_post(pk)
                 else:
                     response_body = update_post(request_body)
-                return  self.response(response_body, status.HTTP_200_SUCCESS.value)
+                return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
         return self.response(
             "Requested resource not found",
@@ -85,22 +96,22 @@ class JSONServer(HandleRequests):
         if url["requested_resource"] == "user":
             pass
         # Example of deleting a user
-    #         if pk != 0:
-    #             successfully_deleted = delete_user(pk)
-    #             if successfully_deleted:
-    #                 return self.response(
-    #                     "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
-    #                 )
+        #         if pk != 0:
+        #             successfully_deleted = delete_user(pk)
+        #             if successfully_deleted:
+        #                 return self.response(
+        #                     "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+        #                 )
 
-    #             return self.response(
-    #                 "Requested resource not found",
-    #                 status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-    #             )
+        #             return self.response(
+        #                 "Requested resource not found",
+        #                 status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+        #             )
 
         else:
             return self.response(
                 "Not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
-        )
+            )
 
     def do_POST(self):
         """Handle POST requests from a client"""
@@ -116,7 +127,7 @@ class JSONServer(HandleRequests):
         if url["requested_resource"] == "register":
             response_body = create_user(request_body)
             return self.response(response_body, status.HTTP_201_SUCCESS_CREATED.value)
-        
+
         # Login a user
         elif url["requested_resource"] == "login":
             response_body = login_user(request_body)
@@ -132,6 +143,7 @@ class JSONServer(HandleRequests):
             return self.response(
                 "", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
             )
+
 
 #
 # THE CODE BELOW THIS LINE IS NOT IMPORTANT FOR REACHING YOUR LEARNING OBJECTIVES
